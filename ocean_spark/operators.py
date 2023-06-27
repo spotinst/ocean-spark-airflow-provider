afc0c801c70500ed285045c674609c844856e028
Mathilde Wallon <mathildw@netapp.com>
2023-06-27 10:47:50 +0200

    POC: update operator to be able to pass the app display name

--- ocean_spark/operators.py
+++ ocean_spark/operators.py
@@ -31,6 +31,7 @@ class OceanSparkOperator(BaseOperator):
     template_fields = (
         "app_id",
         "job_id",
+        "app_display_name",
         "config_template_id",
         "config_overrides",
     )
@@ -44,6 +45,7 @@ class OceanSparkOperator(BaseOperator):
         self,
         job_id: str = None,
         app_id: Optional[str] = None,
+        app_display_name: Optional[str] = None,
         config_template_id: Optional[str] = None,
         config_overrides: Optional[Union[Dict, str]] = None,
         conn_id: str = DEFAULT_CONN_NAME,
@@ -66,6 +68,7 @@ class OceanSparkOperator(BaseOperator):
         self.retry_limit = retry_limit
         self.retry_delay = timedelta(seconds=retry_delay)
         self.app_id: Optional[str] = None  # will be set from the API response
+        self.app_display_name: Optional[str] = None
         self._payload_app_id: Optional[str] = app_id
         self.job_id: Optional[str] = job_id
         self.config_template_id: Optional[str] = config_template_id
@@ -95,6 +98,8 @@ class OceanSparkOperator(BaseOperator):
             self.payload["appId"] = self._payload_app_id
         if self.config_template_id is not None:
             self.payload["configTemplateId"] = self.config_template_id
+        if self.app_display_name is not None:
+            self.payload["appDisplayName"] = self.app_display_name
 
         # templated config overrides dict pulled from xcom is a json str
         if self.config_overrides is not None:
