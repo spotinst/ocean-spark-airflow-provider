2d9046ce14080c6af79090cae382275c1e70febd
Arnar Páll Arnarsson <arnar@netapp.com>
2023-09-18 14:45:38 +0000

    Setting up release 1.0.0 (#22)
    * Setting up release 1.0.0

--- ocean_spark/__init__.py
+++ ocean_spark/__init__.py
@@ -2,11 +2,14 @@ from typing import Dict
 import ocean_spark.hooks
 import ocean_spark.operators
 import ocean_spark.extra_links
+import ocean_spark.connect_hook
+import ocean_spark.connect_operator
 
 
 def get_provider_info() -> Dict:
     return {
         "versions": [
+            "1.0.0",
             "0.1.3",
             "0.1.2",
             "0.1.1",
@@ -15,7 +18,10 @@ def get_provider_info() -> Dict:
         "package-name": "ocean-spark-airflow-provider",
         "name": "Ocean for Spark Airflow Provider",
         "description": "Apache Airflow connector for Ocean for Apache Spark",
-        "hook-class-names": ["ocean_spark.hooks.OceanSparkHook"],
+        "hook-class-names": [
+            "ocean_spark.hooks.OceanSparkHook",
+            "ocean_spark.connect_hook.OceanSparkConnectHook",
+        ],
         "connection-types": [
             {
                 "hook-class-name": "ocean_spark.hooks.OceanSparkHook",
