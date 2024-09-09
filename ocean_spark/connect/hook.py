cca9117d2956513f1c795c0d4f40eef8a35f7771
Sigmar Stefansson <sigmar@netapp.com>
2024-09-09 14:08:44 +0000

    support unix sockets (#36)
    * parse version string for comparison
    
    * support unix sockets
    
    * poetry lock
    
    * added pr template

--- ocean_spark/connect/hook.py
+++ ocean_spark/connect/hook.py
@@ -1,7 +1,8 @@
+from multiprocessing import Process
 from typing import Dict, Any
-import asyncio
-from asyncio.events import AbstractEventLoop
-from pyspark.sql import SparkSession
+import grpc  # type: ignore
+from pyspark.sql.connect.session import SparkSession
+from pyspark.sql.connect.client import ChannelBuilder
 
 from airflow import __version__ as airflow_version
 
@@ -11,19 +12,27 @@ from airflow.exceptions import AirflowException
 
 from urllib.parse import urljoin
 
-from requests import exceptions as requests_exceptions
-
-from ocean_spark.response import ApiResponse
-
 from ocean_spark.connect.inverse_websockify import Proxy
-import threading
 
 API_HOST = "wss://api.spotinst.io/ocean/spark/"
 FE_HOST = "https://console.spotinst.com/ocean/spark/"
 
 USER_AGENT_HEADER = {"user-agent": f"airflow-{airflow_version}"}
 
-DEFAULT_CONN_NAME = "ocean_spark_default"
+DEFAULT_CONN_NAME = "ocean_spark_connect_default"
+
+
+class OceanChannelBuilder(ChannelBuilder):
+    def __init__(self, url: str, bind_address: str):
+        super().__init__(url)
+        self._bind_address = bind_address
+
+    def toChannel(self) -> grpc.Channel:
+        if self._bind_address.startswith("/"):
+            channel = grpc.insecure_channel("unix:" + self._bind_address)
+        else:
+            channel = super().toChannel()
+        return channel
 
 
 class OceanSparkConnectHook(BaseHook):
@@ -35,7 +44,6 @@ class OceanSparkConnectHook(BaseHook):
     def __init__(
         self,
         ocean_spark_connect_conn_id: str = "ocean_spark_connect_default",
-        sql: str = "select 1",
     ):
         super().__init__()
         self.conn_id = ocean_spark_connect_conn_id
@@ -43,14 +51,7 @@ class OceanSparkConnectHook(BaseHook):
         self.token = self.conn.password
         self.cluster_id = self.conn.host
         self.account_id = self.conn.login
-        self.app_id = self.conn.port
-        self.sql = sql
-        self.spark = SparkSession.builder.remote("sc://localhost").getOrCreate()
-
-    def inverse_websockify(self, url: str, loop: AbstractEventLoop) -> None:
-        proxy = Proxy(url, self.token)
-        loop.run_until_complete(proxy.start())
-        loop.run_forever()
+        self.app_id = self.conn.schema
 
     def execute(self, sql: str) -> None:
         path = urljoin(
@@ -58,21 +59,31 @@ class OceanSparkConnectHook(BaseHook):
             f"cluster/{self.cluster_id}/app/{self.app_id}/connect?accountId={self.account_id}",
         )
 
-        loop = asyncio.get_event_loop()
-        my_thread = threading.Thread(target=self.inverse_websockify, args=(path, loop))
-        my_thread.start()
+        self.log.info(f"Starting inverse websockify {path}")
+        _proxy = Proxy(path, self.token, -1, "0.0.0.0", -1)
+        _process = Process(target=_proxy.inverse_websockify, args=())
+        _process.start()
 
+        self.log.info(f"Starting Spark session on {_proxy.addr}")
+        channel_builder = OceanChannelBuilder(
+            f"sc://localhost:{_proxy.port}", _proxy.addr
+        )
+        spark = SparkSession.Builder().channelBuilder(channel_builder).getOrCreate()
         try:
-            self.spark.sql(sql).show()
+            self.log.info(f"Executing SQL: {sql}")
+            spark.sql(sql).show()
+            self.log.info("SQL executed successfully")
         except Exception as e:
             self.log.error(e)
             raise AirflowException(e)
         finally:
-            loop.stop()
-            my_thread.join()
+            self.log.info("Stopping Spark session")
+            spark.stop()
+            _process.kill()
+            self.log.info("Job done")
 
     def kill_task(self) -> None:
-        self.spark.stop()
+        pass
 
     def get_app_page_url(self) -> str:
         url = urljoin(
